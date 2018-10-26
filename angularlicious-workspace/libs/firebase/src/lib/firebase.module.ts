6f0d2669dbe6c205d9d97a3ee30f6dd50c47646d
Matt Vaughn <matt.vaughn@buildmotion.com>
2018-10-25 21:05:05 -0600

    add firebase lib to the workspace

---
+++ angularlicious-workspace/libs/firebase/src/lib/firebase.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Route } from '@angular/router';
+
+export const firebaseRoutes: Route[] = [];
+@NgModule({
+  imports: [CommonModule, RouterModule]
+})
+export class FirebaseModule {}
