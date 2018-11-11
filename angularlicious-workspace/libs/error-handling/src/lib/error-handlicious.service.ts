05c40d1145ecf4f2602ddbbccea59c622b4ec927
Matt Vaughn <matt.vaughn@buildmotion.com>
2018-11-10 21:06:30 -0700

    feat(error-handling) update to extends; implement handleError method

--- angularlicious-workspace/libs/error-handling/src/lib/error-handlicious.service.ts
+++ angularlicious-workspace/libs/error-handling/src/lib/error-handlicious.service.ts
@@ -1,14 +1,30 @@
 import { Injectable, ErrorHandler } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ErrorHandliciousService implements ErrorHandler {
+export class ErrorHandliciousService extends ErrorHandler {
  
 
-  constructor() { }
+  constructor() {
+    super()
+   }
 
-  handleError(error: any): void {
-    throw new Error("Method not implemented.");
-  }
+  handleError(error: Error | HttpErrorResponse):any {
+
+    // use the [super] call to keep default error handling functionality --> console;
+    super.handleError(error);
+
+    // A. HANDLE ERRORS FROM HTTP
+    if (error instanceof HttpErrorResponse) {
+        if (error.error instanceof ErrorEvent) {
+            // A.1: A client-side or network error occurred. Handle it accordingly.
+        } else {
+            // A.2: The API returned an unsuccessful response (i.e., 400, 401, 403, etc.).
+        }
+    } else {
+    // B. HANDLE A GENERALIZED ERROR FROM THE APPLICATION/CLIENT;
+    }
+}
 }
