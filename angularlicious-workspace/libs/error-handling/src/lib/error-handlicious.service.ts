9f0551327f6c101b72a54f23935720124ca3a6bf
Matt Vaughn <matt.vaughn@buildmotion.com>
2018-11-10 21:01:22 -0700

    feat(error-handling) implement ErrorHandler from ng core

--- angularlicious-workspace/libs/error-handling/src/lib/error-handlicious.service.ts
+++ angularlicious-workspace/libs/error-handling/src/lib/error-handlicious.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ErrorHandler } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ErrorHandliciousService {
+export class ErrorHandliciousService implements ErrorHandler {
+ 
 
   constructor() { }
+
+  handleError(error: any): void {
+    throw new Error("Method not implemented.");
+  }
 }
