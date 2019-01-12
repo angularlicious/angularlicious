fdab3c0dffccc92b89662a695db01ac72308c234
Matt Vaughn <matt.vaughn@buildmotion.com>
2019-01-12 14:51:48 -0700

    fix(rules-engine) remove logging from ctro

--- angularlicious-workspace/libs/firebase/src/lib/firebase.module.ts
+++ angularlicious-workspace/libs/firebase/src/lib/firebase.module.ts
@@ -11,12 +11,7 @@ import { AngularliciousFoundationModule } from '@angularlicious/foundation';
 export const firebaseRoutes: Route[] = [];
 
 const firebaseOptions = {
-  apiKey: 'AIzaSyCBh0mZ95iKKx2Jt_f4bVEChUWWXw26ulw',
-  authDomain: 'angularlicious.firebaseapp.com',
-  databaseURL: 'https://angularlicious.firebaseio.com',
-  projectId: 'angularlicious',
-  storageBucket: 'angularlicious.appspot.com',
-  messagingSenderId: '296251746847'
+ 
 };
 
 @NgModule({
